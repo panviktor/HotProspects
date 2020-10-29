53e7b45812890ec093b0bbf96412bd92bcaaca90
SwiftViktor <panafviktor@gmail.com>
2020-10-29 21:47:13 +0300

    Initial Commit

---
+++ HotProspects/App/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Viktor on 25.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
